3fac16b62a56d89721154a088e0977b600d82e1e
AnnaHavryliuk <myaddress2@mail.ru>
2016-09-11 20:37:04 +0300

    add files

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 var gulp = require('gulp')
   , sass = require('gulp-sass')
-  , wiredep = require('wiredep').stream;
+  , runSequence = require('run-sequence')
+  , cleanCSS = require('gulp-clean-css')
+  , uglify = require('gulp-uglify')
+  , wiredep = require('wiredep').stream
+  , useref = require('gulp-useref');
 
 gulp.task('bower', function () {
-  gulp.src('index.html')
+  gulp.src('src/*.html')
     .pipe(wiredep({
-      js: ['bower_components/underscore/underscore.min.js']
+      css: ['../bower_components/normalize-css/normalize.css'],
+      js: ['../bower_components/underscore/underscore-min.js']
     }))
-    .pipe(gulp.dest('./'));
+    .pipe(gulp.dest('src'));
 });
 
 gulp.task('sass', function () {
@@ -16,6 +21,32 @@ gulp.task('sass', function () {
     .pipe(gulp.dest('src/css'))
 });
 
+gulp.task('useref', function() {
+  return gulp.src('src/**/*.html')
+  .pipe(useref())
+  .pipe(gulp.dest('./'))
+});
+
+gulp.task('css:compress', function () {
+    return gulp.src('css/main.min.css')
+      .pipe(cleanCSS())
+      .pipe(gulp.dest('css'));
+});
+
+gulp.task('js:compress', function () {
+   gulp.src('js/main.min.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('js'));
+});
+
+gulp.task('compress', ['css:compress', 'js:compress']);
+
+gulp.task('build', function() {
+  runSequence('useref', 'compress');
+});
+
 gulp.task('watch', function () {
   gulp.watch('src/scss/**/*.scss', ['sass']);
+  gulp.watch('src/css/**/*.css', ['build']);
+  gulp.watch('src/js/**/*.js', ['build']);
 });
--- src/js/main.js
+++ src/js/main.js
@@ -1,64 +1,66 @@
-'use strict'
-onload = function () {
-  var get = function (url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url, true);
+(function() {
+  'use strict'
+  window.onload = function () {
+    var get = function (url, callback) {
+      var xhr = new XMLHttpRequest();
+      xhr.open("GET", url, true);
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState != xhr.DONE) return;
+      xhr.onreadystatechange = function () {
+        if (xhr.readyState != xhr.DONE) return;
 
-      var status = xhr.status;
-      var headers = xhr.getAllResponseHeaders();
-      var text = xhr.responseText;
+        var status = xhr.status;
+        var headers = xhr.getAllResponseHeaders();
+        var text = xhr.responseText;
 
-      callback(status, headers, text);
-    }
-
-    xhr.send();
-  }
-
-  var appendImage = function (url) {
-    var imgEl = document.createElement('img');
-
-    imgEl.src = url;
-
-    imgEl.onerror = function () {
-      imgEl.classList.add('invisible');
-    }
-
-    document.getElementById('images').appendChild(imgEl);
-  }
-
-  var getImages = function (params) {
-    var url = 'https://www.reddit.com/r/pics.json?'
-      , category
-      , limit = 100;
-
-    if (!_.isEmpty(params)) {
-
-      if (params.hasOwnProperty('category') && params.category !== 'pics') {
-        category = params.category;
-        url = url.replace('.json?', '/search.json?q=' + category + '&');
+        callback(status, headers, text);
       }
 
-      if (params.hasOwnProperty('limit')) {
-        limit = params.limit;
-      }
+      xhr.send();
     }
 
-    url += 'limit=' + limit;
+    var appendImage = function (url) {
+      var imgEl = document.createElement('img');
 
-    get(url, function (status, headers, body) {
-      var response = JSON.parse(body);
+      imgEl.src = url;
 
-      _.each(response.data.children, function (child) {
-        var url = child.data.url;
+      imgEl.onerror = function () {
+        imgEl.classList.add('invisible');
+      }
 
-        appendImage(url);
+      document.getElementById('images').appendChild(imgEl);
+    }
+
+    var getImages = function (params) {
+      var url = 'https://www.reddit.com/r/pics.json?'
+        , category
+        , limit = 100;
+
+      if (!_.isEmpty(params)) {
+
+        if (params.hasOwnProperty('category') && params.category !== 'pics') {
+          category = params.category;
+          url = url.replace('.json?', '/search.json?q=' + category + '&');
+        }
+
+        if (params.hasOwnProperty('limit')) {
+          limit = params.limit;
+        }
+      }
+
+      url += 'limit=' + limit;
+
+      get(url, function (status, headers, body) {
+        var response = JSON.parse(body);
+
+        _.each(response.data.children, function (child) {
+          var url = child.data.url;
+
+          appendImage(url);
+        });
       });
+    }
+    document.getElementById('get-images').addEventListener('click', function () {
+      getImages({category: 'cats', limit: 5});
     });
   }
-  document.getElementById('get-images').addEventListener('click', function () {
-    getImages({category: 'cats', limit: 5});
-  });
-}
+})();
